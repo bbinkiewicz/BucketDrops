ebd15c75d73d7bcfb571426afaf9f5d9fb6baccf
Bartosz Binkiewicz <binekb1@gmail.com>
2017-03-07 12:25:25 +0100

    Toolbar and background added using Glide.

--- app/src/main/java/com/example/pc/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/example/pc/bucketdrops/ActivityMain.java
@@ -2,6 +2,10 @@ package com.example.pc.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar mtoolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(mtoolbar);
+        initBackgroundImage();
+    }
+
+    private void initBackgroundImage(){
+
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(background);
     }
 }
